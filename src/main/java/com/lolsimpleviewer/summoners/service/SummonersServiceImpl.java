651e210d72e6806333ca592c6f63924b21d6544d
anwjrrp33 <anwjrrp33@gmail.com>
2022-01-07 18:26:40 +0900

    Summoners v4

---
+++ src/main/java/com/lolsimpleviewer/summoners/service/SummonersServiceImpl.java
@@ -0,0 +1,32 @@
+package com.lolsimpleviewer.summoners.service;
+
+import com.lolsimpleviewer.summoners.entity.Summoners;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class SummonersServiceImpl implements SummonersService {
+
+    @Value("${API.key}")
+    private String key;
+
+    @Override
+    public Summoners getDetail(String name) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/")
+                .path(name.replaceAll(" ", "%20"))
+                .queryParam("api_key", key);
+
+        // System.out.println(restTemplate.exchange(builder.toUriString(), HttpMethod.GET, new HttpEntity<>(new HttpHeaders()), String.class));
+
+        return restTemplate.getForObject(builder.toUriString(), Summoners.class);
+    }
+}
